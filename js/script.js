f316a52e4b2b5852e5175fe0fc6891deb76b34b1
maldrek3103 <cpisanu96@gmail.com>
2021-07-08 16:41:48 +0200

    Made general setup

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Raccogliere i dati inseriti nel form
+// Calcolare il prezzo del biglietto in base alla percorrenza
+// Applicare uno sconto in base all'età
+// Calcolare il totale
+// Stampare i dati nei rispettivi campi
+
+// Elementi del form
+var getFullName = document.getElementById('fullname');
+var getDistance = document.getElementById('distance');
+var getAge = document.getElementById('age');
+var button = document.getElementById('calculate-ticket');
+
+// Elementi biglietto
+var printName = document.getElementById('ticket-name');
+var printDiscount = document.getElementById('applied-discount');
+var printTrainNumber = document.getElementById('train-number');
+var printCarriage = document.getElementById('carriage');
+var printPrice = document.getElementById('price');
+
+// =============== TEST WORKING ===============
+
+// console.log(getFullName);
+// console.log(getDistance);
+// console.log(getAge);
+// console.log(printName);
+// console.log(printDiscount);
+// console.log(printTrainNumber);
+// console.log(printCarriage);
+// console.log(printPrice);
+
+//==============================================
+
+// Evento al click del button
+button.addEventListener('click', function () {
+
+    // Esegui al click del button
+
+
+})
